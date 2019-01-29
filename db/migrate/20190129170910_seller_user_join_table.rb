a182c2473c4b6f3d21d9d2b67676e88b94fa140c
Josh5k <jking57@academic.rrc.ca>
2019-01-29 11:11:20 -0600

    Added users_sellers join table migration

---
+++ db/migrate/20190129170910_seller_user_join_table.rb
@@ -0,0 +1,7 @@
+class SellerUserJoinTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users_sellers, :id => false do |t|
+      t.integer :user_id
+      t.integer :seller_id
+  end
+end
